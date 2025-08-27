03d1d0ee5e92a20673cabf9d674cd1f47ec78245
JoaoGabAntunes <jgabridias@gmail.com>
2025-08-26 22:37:44 -0300

    Jogo criado

---
+++ index.js
@@ -0,0 +1,32 @@
+const min = 1;
+const max = 100;
+const answer = Math.floor(Math.random() * (max - min + 1)) ;
+let running = true;
+let guess;
+let attempts = 0;
+
+while(running){
+    guess = window.prompt(`Guess a number between ${min} and ${max}`);
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+    }
+    else if(guess < min || guess > max ){
+        window.alert("Please enter a valid number");
+    } 
+    else{
+        attempts++;
+         if(guess < answer){
+            
+            window.alert("TOO LOW! TRY AGAIN!");
+        }else if(guess > answer){
+            
+            window.alert("TOO HIGH! TRY AGAIN!");
+        } else{
+            window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts`);
+            running = false;
+        }
+    
+    }
+}
